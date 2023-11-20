e000968889f256351f2e93c3e09e598ab8bb8974
Belayet Riad <belayetriadbd@gmail.com>
2023-11-21 00:20:11 +0600

    Update Layoutes and template conversion mastering in core

--- src/navigation/index.ts
+++ src/navigation/index.ts
@@ -1,19 +1,23 @@
 // ** Icon imports
 import InventoryIcon from '@mui/icons-material/Inventory'
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
+import WebIcon from '@mui/icons-material/Web'
+import { SvgIconTypeMap } from '@mui/material'
+import { OverridableComponent } from '@mui/material/OverridableComponent'
 import HomeOutline from 'mdi-material-ui/HomeOutline'
 
-const navigation = [
-  {
-    title: 'Project Components',
-    icon: HomeOutline,
-    path: '/projects-components'
-  },
-  {
-    title: 'Projects',
-    icon: ShoppingCartIcon,
-    path: '/projects'
-  },
+type TNavigation = {
+  title: string
+  icon?: OverridableComponent<SvgIconTypeMap>
+  path: string
+}
+const navigation: TNavigation[] = [
+  // {
+  //   title: 'Projects',
+  //   icon: ShoppingCartIcon,
+  //   path: '/projects',
+  //   component: 'button'
+  // },
+
   {
     title: 'Website Component Categories',
     icon: InventoryIcon,
@@ -21,8 +25,13 @@ const navigation = [
   },
   {
     title: 'Website Component',
-    icon: InventoryIcon,
+    icon: WebIcon,
     path: '/website-component'
+  },
+  {
+    title: 'Project Components',
+    icon: HomeOutline,
+    path: '/projects-components'
   }
 ]
 
--- src/pages/website-component-categories/index.tsx
+++ src/pages/website-component-categories/index.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from 'react'
 import AppLayout from 'src/layouts/AppLayout'
-import WebsiteComponentCategoriesComponent from 'src/views/website-component-categories/WebsiteComponentCategoriesComponent'
+import WebsiteComponentCategoriesComponent from 'src/views/website-component-categories/WebsiteComponentCategories.component'
 
 const WebsiteComponentCategoriesPage = () => {
   return <WebsiteComponentCategoriesComponent></WebsiteComponentCategoriesComponent>
