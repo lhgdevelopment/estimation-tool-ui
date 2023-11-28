930330ba4bdc28372da8228850ff05c18f0f2772
Belayet Riad <belayetriadbd@gmail.com>
2023-11-29 00:03:12 +0600

    Problems and goals module

---
+++ src/views/problems-and-goals/ProblemsAndGoals.component.tsx
@@ -0,0 +1,36 @@
+import { Box } from '@mui/material'
+import { useState } from 'react'
+import ProblemsAndGoalsFormComponent from './form/ProblemsAndGoalsForm.component'
+import ProblemsAndGoalsListComponent from './list/ProblemsAndGoalsList.component'
+
+export default function ProblemsAndGoalsComponent() {
+  const [editDataId, setEditDataId] = useState<null | string>(null)
+  const [listData, setListData] = useState<any>([])
+  const [editData, setEditData] = useState<any>({})
+
+  return (
+    <>
+      <Box className='container grid px-6 mx-auto'>
+        <Box component={'h1'} className='mt-5 mb-4 text-xl font-semibold text-gray-600 dark:text-gray-300'>
+          Project Component
+        </Box>
+        <ProblemsAndGoalsFormComponent
+          setEditDataId={setEditDataId}
+          editDataId={editDataId}
+          listData={listData}
+          setListData={setListData}
+          editData={editData}
+          setEditData={setEditData}
+        />
+        <ProblemsAndGoalsListComponent
+          setEditDataId={setEditDataId}
+          editDataId={editDataId}
+          listData={listData}
+          setListData={setListData}
+          editData={editData}
+          setEditData={setEditData}
+        />
+      </Box>
+    </>
+  )
+}
