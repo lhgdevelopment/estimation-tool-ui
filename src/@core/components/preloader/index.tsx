809e8270876d3217b144657cc1edbc0c259e1023
Belayet Riad <belayetriadbd@gmail.com>
2023-12-26 01:18:07 +0600

    Update Preloader and fix issues

--- src/@core/components/preloader/index.tsx
+++ src/@core/components/preloader/index.tsx
@@ -2,10 +2,11 @@
 import Box from '@mui/material/Box'
 import { useEffect, useState } from 'react'
 
-type TPreloade = {
+type TPreloaderProps = {
   close: boolean
 }
-const Preloader = (props: TPreloade) => {
+
+const Preloader = (props: TPreloaderProps) => {
   const { close } = props
   const [progress, setProgress] = useState(0)
   const [counter, setCounter] = useState(0)
@@ -20,8 +21,12 @@ const Preloader = (props: TPreloade) => {
       const elapsedTime = currentTime - startTime
       const newProgress = (elapsedTime / duration) * 100
 
+      if (newProgress < 100) {
+        // Only update counter when progress is not complete
+        setCounter(Math.floor(newProgress))
+      }
+
       setProgress(newProgress)
-      setCounter(Math.floor(newProgress))
 
       if (elapsedTime < duration && !closing) {
         requestAnimationFrame(updateProgress)
@@ -55,42 +60,58 @@ const Preloader = (props: TPreloade) => {
         opacity: closing ? 0 : 1,
         pointerEvents: closing ? 'none' : 'auto',
         transition: 'opacity 0.5s ease',
+        '& .percent': {
+          position: 'relative'
+        },
         '& svg': {
-          width: '114px',
-          height: '114px'
+          position: 'relative',
+          width: '210px',
+          height: '210px',
+          transform: 'rotate(-90deg)'
         },
-        '& .bg': {
-          fill: '#1a49b0',
-          strokeWidth: '10px',
-          stroke: '#1A2C34'
-        },
-        '& .meter': {
+        '& circle': {
+          width: '100%',
+          height: '100%',
           fill: 'none',
-          strokeWidth: '10px',
+          stroke: '#f0f0f0',
+          strokeWidth: '10',
           strokeLinecap: 'round',
-          transform: 'rotate(-90deg)',
-          transformOrigin: '50% 50%',
-          strokeDasharray: '360',
-          strokeDashoffset: 360 - 360 * (progress / 100),
-          stroke: '#7e3af2',
-          animation: closing ? 'none' : 'progress 1s ease-out'
+
+          '&:last-of-type': {
+            strokeDasharray: '625px',
+            strokeDashoffset: `calc(625px - (625px * ${counter}) / 100)`,
+            stroke: '#3498db'
+          }
         },
-        '& .counter': {
-          fill: '#fff',
-          fontSize: '24px',
-          dominantBaseline: 'middle',
-          textAnchor: 'middle'
+        '& .number': {
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          fontWeight: '600',
+          fontSize: '3.5rem',
+          '& h3': {
+            color: '#fff',
+            '& span': {
+              fontSize: '2rem'
+            }
+          }
         }
       }}
     >
       <Box>
-        <svg>
-          <circle className='bg' cx='57' cy='57' r='52' />
-          <circle className='meter' cx='57' cy='57' r={52} />
-          <text className='counter' x='57' y='57'>
-            {counter}%
-          </text>
-        </svg>
+        <div className='percent'>
+          <svg>
+            <circle cx='105' cy='105' r='100'></circle>
+            <circle cx='105' cy='105' r='100' style={{ '--percent': `${counter}%` }}></circle>
+          </svg>
+          <div className='number'>
+            <h3>
+              {counter}
+              <span>%</span>
+            </h3>
+          </div>
+        </div>
       </Box>
     </Box>
   )
--- src/views/project-sow/details/ProjectSOW.details.component.tsx
+++ src/views/project-sow/details/ProjectSOW.details.component.tsx
@@ -2,23 +2,30 @@ import { Box, Typography } from '@mui/material'
 import { useRouter } from 'next/router'
 import { Fragment, useEffect, useState } from 'react'
 import ReactMarkdown from 'react-markdown'
+import Preloader from 'src/@core/components/preloader'
 import apiRequest from 'src/@core/utils/axios-config'
 
 export default function DetailsDataDetailsComponent() {
   const router = useRouter()
+  console.log(router?.query['id'])
+
+  const [preload, setPreload] = useState<boolean>(false)
   const [detailsData, setDetailsData] = useState<any>({})
   const getDetails = () => {
+    setPreload(true)
     apiRequest.get(`/project-summery/${router?.query['id']}`).then(res => {
       setDetailsData(res.data)
+      setPreload(false)
     })
   }
 
   useEffect(() => {
     getDetails()
-  }, [])
+  }, [router?.query['id']])
 
   return (
     <Fragment>
+      {!!preload && <Preloader close={!preload} />}
       <Box sx={{ p: 5 }}>
         <Box sx={{ mt: 5 }}>
           <Box>
