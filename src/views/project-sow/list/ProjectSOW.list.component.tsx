e2a1eceac7ea21ce49883ae967ba5306d74f7f75
Belayet Riad <belayetriadbd@gmail.com>
2023-12-30 02:59:42 +0600

    Bug fixing from Bughard

--- src/views/project-sow/list/ProjectSOW.list.component.tsx
+++ src/views/project-sow/list/ProjectSOW.list.component.tsx
@@ -84,7 +84,7 @@ export default function ProjectSOWListComponent(props: TProjectSOWComponent) {
             <thead>
               <tr className='text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800'>
                 <th className='px-4 py-3'>Project</th>
-                <th className='px-4 py-3'>Summary Text</th>
+                <th className='px-4 py-3'>Summary</th>
                 <th className='px-4 py-3'>Created By</th>
                 <th className='px-4 py-3'>Created At</th>
                 <th className='px-4 py-3 text-right'>Actions</th>
