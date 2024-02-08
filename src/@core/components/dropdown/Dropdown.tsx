601c70551eb76642a2f592b14d99f6501fd482a8
Belayet Riad <belayetriadbd@gmail.com>
2024-02-08 22:55:18 +0600

    Make Dropdown Searchable and design customize

--- src/@core/components/dropdown/Dropdown.tsx
+++ src/@core/components/dropdown/Dropdown.tsx
@@ -1,3 +1,5 @@
+import SearchIcon from '@mui/icons-material/Search'
+import { InputAdornment, ListSubheader, TextField } from '@mui/material'
 import FormControl from '@mui/material/FormControl'
 import MenuItem from '@mui/material/MenuItem'
 import Select, { SelectProps } from '@mui/material/Select'
@@ -10,6 +12,8 @@ interface ISelectProps extends SelectProps {
   label?: string
   url?: string
   isEnumField?: boolean
+  searchable?: boolean
+  searchPlaceholder?: string
   optionConfig?: TOptionConfig
   enumList?: { title: string; id: string | number }[]
 }
@@ -22,26 +26,33 @@ export function Dropdown(props: ISelectProps) {
     enumList,
     optionConfig = { title: 'name', id: '_id' },
     multiple = false,
+    searchable = true,
+    searchPlaceholder = 'Type to search...',
     value,
     onChange,
     id = 'demo-simple-select-label',
     ...otherProps
   } = props
 
-  const [menuItems, setMenuItems] = useState<{ title: string; id: string | number }[]>([])
+  const [optionItems, setOptionItems] = useState<Record<string | number, any>[]>([])
+  const [initialOptionList, setInitialOptionList] = useState<Record<string | number, any>[]>([])
+
+  const [searchText, setSearchText] = useState('')
 
   const getList = () => {
-    if (!isEnumField && url != '') {
+    if (!isEnumField && url !== '') {
       apiRequest.get(`/${url}`).then(res => {
-        setMenuItems(
+        const fetchedOptions =
           res.data?.map((item: any) => ({
             title: item?.[optionConfig?.title],
             id: item?.[optionConfig?.id]
           })) || []
-        )
+        setOptionItems(fetchedOptions)
+        setInitialOptionList(fetchedOptions)
       })
     } else if (enumList) {
-      setMenuItems([...enumList])
+      setOptionItems([...enumList])
+      setInitialOptionList([...enumList])
     }
   }
 
@@ -55,6 +66,18 @@ export function Dropdown(props: ISelectProps) {
     }
   }, [url])
 
+  const containsText = (text: string, searchText: string) =>
+    text?.toString().toLowerCase().indexOf(searchText?.toString().toLowerCase()) > -1
+
+  useEffect(() => {
+    setOptionItems(initialOptionList.filter(item => containsText(item['title'], searchText)))
+  }, [searchText])
+
+  const selectOnOpen = () => {
+    setOptionItems(initialOptionList)
+    setSearchText('')
+  }
+
   return (
     <FormControl fullWidth>
       <Select
@@ -64,16 +87,43 @@ export function Dropdown(props: ISelectProps) {
         multiple={multiple}
         sx={{ mt: 1, height: 38 }}
         displayEmpty
-        autoWidth
+        fullWidth
       >
+        {searchable && (
+          <ListSubheader
+            sx={{
+              pt: 1
+            }}
+          >
+            <TextField
+              size='small'
+              autoFocus
+              placeholder={searchPlaceholder}
+              fullWidth
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position='start'>
+                    <SearchIcon />
+                  </InputAdornment>
+                )
+              }}
+              onChange={e => setSearchText(() => e.target.value)}
+              onKeyDown={e => {
+                if (e.key !== 'Escape') {
+                  e.stopPropagation()
+                }
+              }}
+            />
+          </ListSubheader>
+        )}
         {placeholder && (
           <MenuItem value='' disabled>
             {placeholder}
           </MenuItem>
         )}
-        {menuItems?.map((menuItem: any, index: number) => (
-          <MenuItem value={menuItem.id} key={index}>
-            {menuItem.title}
+        {optionItems?.map((option: any, index: number) => (
+          <MenuItem value={option.id} key={index}>
+            {option.title}
           </MenuItem>
         ))}
       </Select>
