b4dcd5250176567f88fe8dbf53c33da21a415eae
Belayet Riad <belayetriadbd@gmail.com>
2024-02-14 23:58:51 +0600

    Generate Tasks Form from Clickup fetched data ::: Frontend

--- src/views/service-management/service-deliverable-tasks/form/ServiceDeliverableTasks.form.component.tsx
+++ src/views/service-management/service-deliverable-tasks/form/ServiceDeliverableTasks.form.component.tsx
@@ -9,6 +9,7 @@ import dynamic from 'next/dynamic'
 import { Fragment, useEffect, useRef, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { Dropdown, ServiceDropdownTree } from 'src/@core/components/dropdown'
+import Preloader from 'src/@core/components/preloader'
 import { RootState } from 'src/@core/store/reducers'
 import apiRequest from 'src/@core/utils/axios-config'
 import Swal from 'sweetalert2'
@@ -21,9 +22,10 @@ export default function ServiceDeliverableTasksFormComponent(props: TServiceDeli
   const isDark = useSelector((state: RootState) => state.theme.isDark)
   const nameEditorRef = useRef(null)
   const [errorMessage, setErrorMessage] = useState<any>({})
-  const [pullingTaskFromClickup, setPullingTaskFromClickup] = useState(false)
+  const [isPullingTaskFromClickup, setIsPullingTaskFromClickup] = useState(false)
   const [isFatchFromClickUp, setIsFatchFromClickUp] = useState(false)
   const [clickupLink, setClickupLink] = useState('')
+  const [clickupTaskList, setClickupTaskList] = useState<any[]>([])
 
   const defaultData = {
     tasks: [
@@ -110,14 +112,14 @@ export default function ServiceDeliverableTasksFormComponent(props: TServiceDeli
   }
 
   const handleFetchTaskFromClickup = () => {
-    setPullingTaskFromClickup(true)
+    setIsPullingTaskFromClickup(true)
     const data = {
       clickupLink
     }
     apiRequest
       .post('/clickup/list', data)
       .then(res => {
-        console.log(res)
+        setClickupTaskList(res.data)
       })
       .catch(error => {
         setErrorMessage(error?.response?.data?.errors)
@@ -224,44 +226,255 @@ export default function ServiceDeliverableTasksFormComponent(props: TServiceDeli
         </Box>
         {isFatchFromClickUp ? (
           <>
-            <Box sx={{ display: 'flex', gap: 5, mb: 5 }}>
-              <Box sx={{ width: '100%' }}>
-                <label className='block text-sm'>
-                  <span className='text-gray-700 dark:text-gray-400'>Clickup Link</span>
-                  <input
-                    className='block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input'
-                    placeholder='Enter clickup task link'
-                    name='clickupLink'
-                    value={clickupLink}
-                    onChange={e => {
-                      setClickupLink(e.target.value)
+            {!!isPullingTaskFromClickup && <Preloader close={!!clickupTaskList?.length} />}
+            <Box>
+              <Box sx={{ display: 'flex', gap: 5, mb: 5 }}>
+                <Box sx={{ width: '100%' }}>
+                  <label className='block text-sm'>
+                    <span className='text-gray-700 dark:text-gray-400'>Clickup Link</span>
+                    <input
+                      className={`block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input ${
+                        !!clickupTaskList?.length ? 'opacity-50' : ''
+                      }`}
+                      placeholder='Enter clickup task link'
+                      name='clickupLink'
+                      value={clickupLink}
+                      onChange={e => {
+                        setClickupLink(e.target.value)
+                      }}
+                      disabled={!!clickupTaskList?.length}
+                    />
+                    {!!errorMessage?.['clickupLink'] &&
+                      errorMessage?.['clickupLink']?.map((message: any, index: number) => {
+                        return (
+                          <span key={index} className='text-xs text-red-600 dark:text-red-400'>
+                            {message}
+                          </span>
+                        )
+                      })}
+                  </label>
+                </Box>
+              </Box>
+              <Box sx={{ display: 'flex', gap: 5, mb: 5 }}>
+                <Box sx={{ width: '100%', textAlign: 'right' }}>
+                  <button
+                    type='button'
+                    className={`px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-green-600 border border-transparent rounded-lg active:bg-green-600 hover:bg-green-700 focus:outline-none focus:shadow-outline-green ${
+                      !!clickupTaskList?.length ? 'opacity-50' : ''
+                    }`}
+                    onClick={() => {
+                      handleFetchTaskFromClickup()
                     }}
-                  />
-                  {!!errorMessage?.['clickupLink'] &&
-                    errorMessage?.['clickupLink']?.map((message: any, index: number) => {
-                      return (
-                        <span key={index} className='text-xs text-red-600 dark:text-red-400'>
-                          {message}
-                        </span>
-                      )
-                    })}
-                </label>
+                    disabled={!!clickupTaskList?.length}
+                  >
+                    Click Here Fetch Task From Clickup
+                    <BrowserUpdatedIcon sx={{ ml: 2 }} />
+                  </button>
+                </Box>
               </Box>
             </Box>
-            <Box sx={{ display: 'flex', gap: 5, mb: 5 }}>
-              <Box sx={{ width: '100%', textAlign: 'right' }}>
-                <button
-                  type='button'
-                  className='px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-green-600 border border-transparent rounded-lg active:bg-green-600 hover:bg-green-700 focus:outline-none focus:shadow-outline-green'
-                  onClick={() => {
-                    handleFetchTaskFromClickup()
-                  }}
-                >
-                  Click Here Fetch Task From Clickup
-                  <BrowserUpdatedIcon sx={{ ml: 2 }} />
-                </button>
+
+            {!!clickupTaskList?.length && (
+              <Box>
+                <form onSubmit={onSubmit}>
+                  {clickupTaskList.map((clickupTask: any, index: number) => {
+                    return (
+                      <>
+                        <Box>
+                          <Box
+                            sx={{
+                              display: 'flex',
+                              gap: 5,
+                              mb: 5,
+                              flexDirection: 'column',
+                              border: '2px solid #9333ea',
+                              padding: '24px',
+                              borderRadius: '5px'
+                            }}
+                          >
+                            <Box sx={{ width: '100%', display: 'flex', flexDirection: 'column', gap: 5, mb: 5 }}>
+                              <Box sx={{ width: '100%' }}>
+                                <JoditEditor
+                                  ref={nameEditorRef}
+                                  config={{ enter: 'br', theme: isDark ? 'dark' : '' }}
+                                  value={clickupTask.name}
+                                  onBlur={newContent => handleReachText(newContent, 'name', index)}
+                                />
+                                {/* {!!errorMessage?.[`tasks.${index}.name`] &&
+                                  errorMessage?.[`tasks.${index}.name`]?.map((message: any, index: number) => {
+                                    return (
+                                      <span key={index} className='text-xs text-red-600 dark:text-red-400'>
+                                        {String(message).replaceAll('tasks.0.', '')}
+                                      </span>
+                                    )
+                                  })} */}
+                              </Box>
+                              <Box sx={{ width: '100%' }}>
+                                <JoditEditor
+                                  ref={nameEditorRef}
+                                  config={{ enter: 'br', theme: isDark ? 'dark' : '' }}
+                                  value={clickupTask.name}
+                                  onBlur={newContent => handleReachText(newContent, 'name', index)}
+                                />
+                                {/* {!!errorMessage?.[`tasks.${index}.name`] &&
+                                  errorMessage?.[`tasks.${index}.name`]?.map((message: any, index: number) => {
+                                    return (
+                                      <span key={index} className='text-xs text-red-600 dark:text-red-400'>
+                                        {String(message).replaceAll('tasks.0.', '')}
+                                      </span>
+                                    )
+                                  })} */}
+                              </Box>
+                            </Box>
+
+                            {!!clickupTask?.subTasks?.length &&
+                              clickupTask?.subTasks?.map((subTask: any, subTasksIndex: number) => {
+                                return (
+                                  <Box
+                                    sx={{
+                                      display: 'flex',
+                                      gap: 5,
+                                      mb: 5,
+                                      flexDirection: 'column',
+                                      border: '2px solid #9333ea',
+                                      padding: '24px',
+                                      borderRadius: '5px'
+                                    }}
+                                    key={subTasksIndex}
+                                  >
+                                    <Box sx={{ width: '100%', display: 'flex', gap: 5, mb: 5 }}>
+                                      <Box sx={{ width: '100%' }}>
+                                        <label className='block text-sm'>
+                                          <span className='text-gray-700 dark:text-gray-400'>Name</span>
+                                        </label>
+                                        <Box
+                                          className='block text-sm'
+                                          sx={{
+                                            width: '100%',
+                                            display: 'flex',
+                                            alignItems: 'center',
+                                            justifyContent: 'center'
+                                          }}
+                                        >
+                                          <Box
+                                            sx={{
+                                              width: '100%',
+                                              display: 'flex',
+                                              flexDirection: 'column',
+                                              gap: 5,
+                                              mb: 5
+                                            }}
+                                          >
+                                            <Box sx={{ width: '100%' }}>
+                                              <JoditEditor
+                                                ref={nameEditorRef}
+                                                config={{ enter: 'br', theme: isDark ? 'dark' : '' }}
+                                                value={subTask.name}
+                                                onBlur={newContent => handleReachText(newContent, 'name', index)}
+                                              />
+                                              {!!errorMessage?.[`tasks.${index}.name`] &&
+                                                errorMessage?.[`tasks.${index}.name`]?.map(
+                                                  (message: any, index: number) => {
+                                                    return (
+                                                      <span
+                                                        key={index}
+                                                        className='text-xs text-red-600 dark:text-red-400'
+                                                      >
+                                                        {String(message).replaceAll('tasks.0.', '')}
+                                                      </span>
+                                                    )
+                                                  }
+                                                )}
+                                            </Box>
+                                            <Box sx={{ display: 'flex', gap: 5, mb: 5 }}>
+                                              <Box sx={{ width: '50%' }}>
+                                                <label className='block text-sm'>
+                                                  <span className='text-gray-700 dark:text-gray-400'>Hour</span>
+                                                  <input
+                                                    className='block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input'
+                                                    placeholder='Examples: 50.00'
+                                                    name='cost'
+                                                    value={subTask.cost}
+                                                    onChange={e => {
+                                                      handleChange(e, index)
+                                                    }}
+                                                  />
+                                                  {!!errorMessage?.[`tasks.${index}.cost`] &&
+                                                    errorMessage?.[`tasks.${index}.cost`]?.map(
+                                                      (message: any, index: number) => {
+                                                        return (
+                                                          <span
+                                                            key={index}
+                                                            className='text-xs text-red-600 dark:text-red-400'
+                                                          >
+                                                            {String(message).replaceAll('tasks.0.', '')}
+                                                          </span>
+                                                        )
+                                                      }
+                                                    )}
+                                                </label>
+                                              </Box>
+                                              <Box sx={{ width: '50%' }}>
+                                                <label className='block text-sm'>
+                                                  <span className='text-gray-700 dark:text-gray-400'>Description</span>
+                                                  <input
+                                                    className='block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input'
+                                                    placeholder='Examples: Company logo for header'
+                                                    name='description'
+                                                    value={subTask.description}
+                                                    onChange={e => {
+                                                      handleChange(e, index)
+                                                    }}
+                                                  />
+                                                  {!!errorMessage?.[`tasks.${index}.description`] &&
+                                                    errorMessage?.[`tasks.${index}.description`]?.map(
+                                                      (message: any, index: number) => {
+                                                        return (
+                                                          <span
+                                                            key={index}
+                                                            className='text-xs text-red-600 dark:text-red-400'
+                                                          >
+                                                            {String(message).replaceAll('tasks.0.', '')}
+                                                          </span>
+                                                        )
+                                                      }
+                                                    )}
+                                                </label>
+                                              </Box>
+                                            </Box>
+                                          </Box>
+                                        </Box>
+                                      </Box>
+                                    </Box>
+                                  </Box>
+                                )
+                              })}
+                          </Box>
+                        </Box>
+                      </>
+                    )
+                  })}
+
+                  <Box className='my-4 text-right'>
+                    <button
+                      onClick={onClear}
+                      type='button'
+                      className='px-4 py-2 mr-3 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-red-600 border border-transparent rounded-lg active:bg-red-600 hover:bg-red-700 focus:outline-none focus:shadow-outline-red'
+                    >
+                      {editDataId ? 'Cancel ' : 'Clear '}
+                      {editDataId ? <ClearIcon /> : <PlaylistRemoveIcon />}
+                    </button>
+                    <button
+                      type='submit'
+                      className='px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-green-600 border border-transparent rounded-lg active:bg-green-600 hover:bg-green-700 focus:outline-none focus:shadow-outline-green'
+                    >
+                      {editDataId ? 'Update ' : 'Save '}
+                      {editDataId ? <EditNoteIcon /> : <AddIcon />}
+                    </button>
+                  </Box>
+                </form>
               </Box>
-            </Box>
+            )}
           </>
         ) : (
           <>
