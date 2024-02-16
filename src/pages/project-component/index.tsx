922cc8c38817308ba7a8071b3ae563bcb96e0ebc
Belayet Riad <belayetriadbd@gmail.com>
2024-02-17 02:32:20 +0600

    Role Permission Creation

--- src/pages/project-component/index.tsx
+++ src/pages/project-component/index.tsx
@@ -1,9 +1,9 @@
 import { ReactNode } from 'react'
 import AppLayout from 'src/layouts/AppLayout'
-import ProjectComponent from 'src/views/project-component/ProjectComponent.component'
+import ProjectComponentComponent from 'src/views/project-component/ProjectComponent.component'
 
 const ProjectComponent = () => {
-  return <ProjectComponent></ProjectComponent>
+  return <ProjectComponentComponent></ProjectComponentComponent>
 }
 ProjectComponent.getLayout = (page: ReactNode) => <AppLayout>{page}</AppLayout>
 export default ProjectComponent
