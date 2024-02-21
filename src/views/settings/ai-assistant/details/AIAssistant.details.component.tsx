1e210a0455270fa362e7e45276cc79c9a1302ab2
Belayet Riad <belayetriadbd@gmail.com>
2024-02-22 01:28:52 +0600

    Ability to configure type wise meeting summery prompt and tie it with meeting summery.:: Frontend

--- src/views/settings/ai-assistant/details/AIAssistant.details.component.tsx
+++ src/views/settings/ai-assistant/details/AIAssistant.details.component.tsx
@@ -41,7 +41,27 @@ export default function AIAssistantDetailsComponent() {
   return (
     <Box sx={{ p: 5 }}>
       <Box>
-        <Box sx={{ display: 'flex', flexDirection: 'column' }}></Box>
+        <Box className='container px-6 mx-auto' sx={{ height: 'calc(100vh - 126px)', position: 'relative' }}>
+          <Box></Box>
+          <Box
+            sx={{
+              position: 'absolute',
+              width: '100%',
+              bottom: '0',
+              left: '0',
+              right: '0'
+            }}
+          >
+            <Box
+              component={'textarea'}
+              id='prompt-textarea'
+              placeholder='Chat Prompt...'
+              rows={1}
+              className='block w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 form-select focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray'
+              sx={{ height: '40px', overflowY: 'hidden' }}
+            ></Box>
+          </Box>
+        </Box>
       </Box>
     </Box>
   )
