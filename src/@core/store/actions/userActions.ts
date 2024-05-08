b7c34cb2ef4b0df3f035df032106264dbc756d4e
Belayet Riad <belayetriadbd@gmail.com>
2024-05-09 02:42:30 +0600

    Fetch List and Table in Master

--- src/@core/store/actions/userActions.ts
+++
@@ -1,9 +0,0 @@
-export const loginUser = (user: any) => ({
-  type: 'LOGIN_USER',
-  payload: user
-})
-
-export const isDarkTheme = (data: boolean) => ({
-  type: 'IS_DARK_THEME',
-  payload: data
-})
