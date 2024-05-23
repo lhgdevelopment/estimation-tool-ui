d64f442c26f0bf460e8e145aa5ff050d20e90f1d
Belayet Riad <belayetriadbd@gmail.com>
2024-05-24 01:50:13 +0600

    Optimization and Refactor

--- src/pages/project/index.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react'
-import AppLayout from 'src/layouts/AppLayout'
-import ProjectComponent from 'src/views/project/Project.component'
-
-const Projects = () => {
-  return <ProjectComponent></ProjectComponent>
-}
-Projects.getLayout = (page: ReactNode) => <AppLayout>{page}</AppLayout>
-export default Projects
