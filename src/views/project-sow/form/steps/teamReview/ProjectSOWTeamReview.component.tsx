2dc79106622ffe64f923cad3a5bcc933ebe840a2
Belayet Riad <belayetriadbd@gmail.com>
2024-07-13 15:46:07 +0600

    Refactor and Optimization SOW all Part

--- src/views/project-sow/form/steps/teamReview/ProjectSOWTeamReview.component.tsx
+++ src/views/project-sow/form/steps/teamReview/ProjectSOWTeamReview.component.tsx
@@ -1,7 +1,77 @@
 import 'md-editor-rt/lib/style.css'
+import { enqueueSnackbar } from 'notistack'
+import { useEffect, useState } from 'react'
+import apiRequest from 'src/@core/utils/axios-config'
 import { TProjectSOWTeamReviewFormComponentProps } from './ProjectSOWTeamReview.decorator'
 import ProjectSOWTeamReviewFormView from './ProjectSOWTeamReview.view'
 
 export default function ProjectSOWTeamReviewFormComponent(props: TProjectSOWTeamReviewFormComponentProps) {
-  return <ProjectSOWTeamReviewFormView {...props}></ProjectSOWTeamReviewFormView>
+  const {
+    projectSOWFormData,
+    setOverviewText,
+    overviewText,
+    errorMessage,
+    setAssociatedUserWithRole,
+    associatedUserWithRole,
+    problemGoalText
+  } = props
+  const [employeeRoleData, setEmployeeRole] = useState<any>([])
+  const [teamUserList, setTeamUserList] = useState<any>([])
+  const getAssociatedUserWithRole = (roleId: number, userId: number) => {
+    setAssociatedUserWithRole((prevState: any) => {
+      if (prevState.some((item: any) => item.employeeRoleId === roleId)) {
+        return prevState?.map((item: any) => {
+          if (item.employeeRoleId === roleId) {
+            return { ...item, associateId: userId }
+          }
+
+          return item
+        })
+      } else {
+        return [...prevState, { employeeRoleId: roleId, associateId: userId }]
+      }
+    })
+  }
+  const getEmployeeRoleList = async () => {
+    await apiRequest
+      .get(`/employee-roles`)
+      .then(res => {
+        setEmployeeRole(
+          res?.data?.map((item: any) => {
+            return { ...item, title: item?.name }
+          })
+        )
+      })
+      .catch(error => {
+        enqueueSnackbar(error?.message, { variant: 'error' })
+      })
+  }
+  const getUserList = async () => {
+    await apiRequest
+      .get(`/associates`)
+      .then(res => {
+        setTeamUserList(res?.data)
+      })
+      .catch(error => {
+        enqueueSnackbar(error?.message, { variant: 'error' })
+      })
+  }
+  useEffect(() => {
+    getEmployeeRoleList()
+    getUserList()
+  }, [])
+
+  return (
+    <ProjectSOWTeamReviewFormView
+      projectSOWFormData={projectSOWFormData}
+      setOverviewText={setOverviewText}
+      overviewText={overviewText}
+      errorMessage={errorMessage}
+      getAssociatedUserWithRole={getAssociatedUserWithRole}
+      associatedUserWithRole={associatedUserWithRole}
+      employeeRoleData={employeeRoleData}
+      teamUserList={teamUserList}
+      problemGoalText={problemGoalText}
+    ></ProjectSOWTeamReviewFormView>
+  )
 }
--- src/views/project-sow/form/steps/teamReview/ProjectSOWTeamReview.decorator.ts
+++ src/views/project-sow/form/steps/teamReview/ProjectSOWTeamReview.decorator.ts
@@ -5,11 +5,19 @@ export type TProjectSOWTeamReviewFormComponentProps = {
   setOverviewText: Dispatch<SetStateAction<string>>
   overviewText: string
   errorMessage: any
+  setAssociatedUserWithRole: Dispatch<SetStateAction<{ employeeRoleId: string; associateId: string }[]>>
+  associatedUserWithRole: any[]
+  problemGoalText: string
 }
 
 export type TProjectSOWTeamReviewFormViewProps = {
+  getAssociatedUserWithRole: (roleId: number, userId: number) => void
   projectSOWFormData: any
-  setOverviewText: Dispatch<SetStateAction<string>>
-  overviewText: string
   errorMessage: any
+  problemGoalText: string
+  overviewText: string
+  setOverviewText: Dispatch<SetStateAction<string>>
+  employeeRoleData: any[]
+  associatedUserWithRole: any[]
+  teamUserList: any[]
 }
