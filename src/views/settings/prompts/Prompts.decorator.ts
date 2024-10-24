46137c0e2bc90594ab53ba12dc2a67a907141098
Raju at LHG <raju@lhgraphics.com>
2024-10-24 21:51:14 +0600

    Reverted Prompt

--- src/views/settings/prompts/Prompts.decorator.ts
+++ src/views/settings/prompts/Prompts.decorator.ts
@@ -1,6 +1,10 @@
-import { Dispatch } from 'react'
+import { Dispatch, SetStateAction } from 'react'
 
 export type TPromptsComponent = {
+  editDataId?: string | null
+  setEditDataId: Dispatch<SetStateAction<string | null>>
+  editData?: any
+  setEditData: Dispatch<any> // Remove the optional '?'
   listData: any[]
   setListData: Dispatch<any>
 }
